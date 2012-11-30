b47f125378cfa945cf442cc5bcf3b31bd88a17e7
Matt Shwery <mshwery@gmail.com>
2012-11-29 21:55:33 -0600

    added omni auth users and identities... easily add twitter/google/facebook sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Gather::Application.routes.draw do
   resources :sources
   resources :articles
+  resources :identities
   match 'manage' => 'articles#manage'
-
+  match "/auth/:provider/callback", to: "sessions#create"
+  match "/auth/failure", to: "sessions#failure"
+  match "/logout", to: "sessions#destroy", :as => "logout"
+  match "/login", to: "sessions#new"
+  
   root :to => 'articles#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121129203035) do
+ActiveRecord::Schema.define(:version => 20121130031105) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -24,9 +24,25 @@ ActiveRecord::Schema.define(:version => 20121129203035) do
     t.datetime "published_at"
   end
 
+  create_table "identities", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+  end
+
   create_table "sources", :force => true do |t|
     t.string   "rss"
     t.string   "url"
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "provider"
+    t.string   "uid"
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
